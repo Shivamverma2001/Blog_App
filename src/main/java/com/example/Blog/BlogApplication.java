80c3e40776740138220737786ad4992f8cc97419
rajendra <rajumahapatra096@gmail.com>
2023-11-30 11:35:07 +0530

    Docker file added for deployment

--- src/main/java/com/example/Blog/BlogApplication.java
+++ src/main/java/com/example/Blog/BlogApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogApplication {
+	public String PORT = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
